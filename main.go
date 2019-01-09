32b1777c8ea9ba55d0d8fff4fa4c14fa25eab0a6
Dalton C <dolphindalt@gmail.com>
2019-01-09 09:38:31 -0700

    Tests

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"log"
+
+	"github.com/Dolphindalt/gorplidar"
 
 	"gocv.io/x/gocv"
 )
@@ -14,7 +17,19 @@ const (
 )
 
 func main() {
-	pc := NewPointCloudFromFile("points.txt")
+	lidar := gorplidar.NewRPLidar("/dev/ttyUSB0", 115200)
+	err := lidar.Connect()
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	err = lidar.StartMotor()
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	pc, err := NewPointCloudFromLidar(lidar)
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
 	pc.toImageSpace()
 	pc.saveAsImage("pointcloud.png", pointScale, scaleFactor)
 	img := gocv.IMRead("pointcloud.png", gocv.IMReadGrayScale)
@@ -26,7 +41,7 @@ func main() {
 	red := color.RGBA{255, 0, 0, 1}
 	lines := ExtractEndpoints("pointcloud.png")
 	for _, l := range lines {
-		gocv.Line(&cimg, image.Pt(int(l.P1.X()), int(l.P1.Y())), image.Pt(int(l.P2.X()), int(l.P2.Y())), red, 3)
+		gocv.Line(&cimg, image.Pt(int(l.P1.X()), int(l.P1.Y())), image.Pt(int(l.P2.X()), int(l.P2.Y())), red, 2)
 	}
 	lines = TranslateLines(lines, scaleFactor, pc.shift)
 	for _, l := range lines {
